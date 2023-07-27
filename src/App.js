8d4da2c2c18b1d78c03044772117a1a136dbd0e4
Dorin <dorin.zahariazd@gmail.com>
2023-07-27 15:35:06 +0300

    Add weather forecast fetching

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
-import Search from './components/Search';
 import './App.css';
+import Search from './components/Search';
+import CurrentWeather from './components/CurrentWeather';
+import axios from 'axios';
+import { useState } from 'react';
 
 function App() {
-  const handleOnSearchChange = () => {};
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [forecastWeather, setForecastWeather] = useState(null);
+
+  const handleOnSearchChange = (searchData) => {
+    fetchCurrentWeather(searchData.lat, searchData.lon, searchData.label);
+    fetchWeatherForecast(searchData.lat, searchData.lon, searchData.label);
+  };
+
+  const fetchCurrentWeather = async (lon, lat, city) => {
+    try {
+      const response = await axios({
+        url: `${process.env.REACT_APP_OPENWEATHER_API_URL}/data/2.5/weather?lon=${lon}&lat=${lat}&appid=${process.env.REACT_APP_OPENWEATHER_API_KEY}&units=metric`,
+      });
+      console.log(response);
+      setCurrentWeather({ city: city, ...response.data });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const fetchWeatherForecast = async (lon, lat, city) => {
+    try {
+      const response = await axios({
+        url: `${process.env.REACT_APP_OPENWEATHER_API_URL}/data/2.5/forecast?lon=${lon}&lat=${lat}&appid=${process.env.REACT_APP_OPENWEATHER_API_KEY}&units=metric`,
+      });
+      setForecastWeather({ city: city, ...response.data });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  console.log(currentWeather);
+
   return (
     <div className='container'>
       <Search onSearchChange={handleOnSearchChange} />
+      {currentWeather && <CurrentWeather data={currentWeather} />}
     </div>
   );
 }
